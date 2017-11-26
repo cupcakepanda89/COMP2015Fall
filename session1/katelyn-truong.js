30b1018fbd33439179d982fca8dc52b69f0c932a
ktruong <cupcakepanda89@users.noreply.github.com>
2017-11-25 17:47:45 -0800

    first commit

---
+++ session1/katelyn-truong.js
@@ -0,0 +1,11 @@
+/**
+ * Created by katelyntruong on 2017-11-07.
+ */
+var w = document.getElementById('cat').clientWidth;
+var h = document.getElementById('cat').clientHeight;
+document.getElementById('bird').width=w;
+document.getElementById('bird').height=h;
+
+
+
+
