d684aa558c17a56ee39ce63fb52958fd98600d6b
cupcakepanda89 <cupcakepanda89@users.noreply.github.com>
2017-12-10 18:36:58 -0800

    do home works and finished up final exam
    upload final exam to drop box

---
+++ truong_katelyn_comp2015_final/final.js
@@ -0,0 +1,13 @@
+// Name: katelyn Truong
+/*
+1. document: the HTML web page
+2. window: the current window or tab
+3. navigator: the browsers itself
+4. style the CSS properties of an element
+ */
+// <!-- Student ID: A00952509
+
+// question1: use DOM 0 to resize the width of the first image on the document to 100
+document.images[0].style.width = '100px';
+
+// question2:
